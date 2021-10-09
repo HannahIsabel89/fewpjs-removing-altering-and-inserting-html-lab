7ca1685abd5628ceb7e856b251013060c9d5859d
Hannah <hannahwoodward@hotmail.com>
2021-10-09 11:39:01 +0800

    Same as other lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+// lab work
+// remove main
+document.querySelector('main').remove();
+// or
+//main.remove();
+
+// new header
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+document.body.appendChild(newHeader);
+
+// add elements to the DOM with innerHTML
+newHeader.innerHTML =  "Hannah is the champion";
+newHeader.className = "victory"
+
+// how to set a h1 with a class and id at the same time
+document.body.innerHTML = `<h1 class="h1" id="h1">Hello</h1>`
